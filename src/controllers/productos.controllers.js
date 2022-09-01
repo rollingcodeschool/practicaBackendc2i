fd55f21289974a1d39cbe8a167d167535afc888a
Emilse Arias <emilsearias@gmail.com>
2022-09-01 11:37:24 -0300

    agregamos el controlador para los productos

---
+++ src/controllers/productos.controllers.js
@@ -0,0 +1,11 @@
+
+export const crearProducto = (req, res)=>{
+    res.send('crear un producto en la base de datos')
+}
+
+export const listarProductos = (req, res)=>{
+    res.send('enviar lista de productos')
+}
+export const obtenerProducto = (req, res)=>{
+    res.send('Aqui envio un objeto producto')
+}
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,14 +1,14 @@
 import {Router} from 'express';
+import { crearProducto, listarProductos, obtenerProducto } from '../controllers/productos.controllers';
 
 const router = Router();
 
 //crear todas las rutas de los productos
-// dominio+ /apicafe + /
-router
-.route('/')
-.get((req, res)=>{
-    res.send('primera peticion get')
-})
+// dominio+ /apicafe + /productos
+router.route('/productos').get(listarProductos).post(crearProducto);
+
+router.route('/productos/:id').get(obtenerProducto).put().delete()
+
 
 export default router;
 
