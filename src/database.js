8390cac2a2800f8a0da26cfe586bd83fa6d5ff2c
Emilse Arias <emilsearias@gmail.com>
2022-09-15 10:14:18 -0300

    agregue la conexion de dani

--- src/database.js
+++ src/database.js
@@ -4,10 +4,17 @@ import mongoose from "mongoose";
 const url = 'mongodb+srv://mdanielacardozo:mtkZQ5FjoyI9ZEIk@clusterdani.7tfbsbj.mongodb.net/cafeteria'; //bdPublicada
 // const url = 'mongodb+srv://comisionc2i:C18PDiw8EH8lfZTr@clustercomision2i.xkgumte.mongodb.net/cafec2i'; //bdPublicada
 
-mongoose.connect(url);
-
-const connection = mongoose.connection;
-
-connection.once('open',()=>{
-    console.log('BD conectada')
-})
+// mongoose.connect(url);
+// const connection = mongoose.connection;
+// connection.once('open',()=>{
+//     console.log('BD conectada')
+// })
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(url);
+        console.log('BD connectada')
+    }catch(error){
+        console.log(error)
+    }
+}
+connectDB();
