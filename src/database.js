32a3bdf44033cdd9030d98c7792471f639082098
Emilse Arias <emilsearias@gmail.com>
2022-09-15 10:22:32 -0300

    comente la segunda opcion de conexion a mongoatlas

--- src/database.js
+++ src/database.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
 // const url = 'mongodb://localhost:27017/cafeteriac2i'; //bd local
-const url = 'mongodb+srv://mdanielacardozo:mtkZQ5FjoyI9ZEIk@clusterdani.7tfbsbj.mongodb.net/cafeteria'; //bdPublicada
-// const url = 'mongodb+srv://comisionc2i:C18PDiw8EH8lfZTr@clustercomision2i.xkgumte.mongodb.net/cafec2i'; //bdPublicada
+// const url = 'mongodb+srv://mdanielacardozo:mtkZQ5FjoyI9ZEIk@clusterdani.7tfbsbj.mongodb.net/cafeteria'; //bdPublicada
+const url = 'mongodb+srv://comisionc2i:C18PDiw8EH8lfZTr@clustercomision2i.xkgumte.mongodb.net/cafec2i'; //bdPublicada
 
-// mongoose.connect(url);
-// const connection = mongoose.connection;
-// connection.once('open',()=>{
-//     console.log('BD conectada')
-// })
-const connectDB = async ()=>{
-    try{
-        await mongoose.connect(url);
-        console.log('BD connectada')
-    }catch(error){
-        console.log(error)
-    }
-}
-connectDB();
+mongoose.connect(url);
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log('BD conectada')
+})
+
+// opcion 2
+// const connectDB = async ()=>{
+//     try{
+//         await mongoose.connect(url);
+//         console.log('BD connectada')
+//     }catch(error){
+//         console.log(error)
+//     }
+// }
+// connectDB();
