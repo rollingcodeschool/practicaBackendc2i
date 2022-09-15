079cbb9979aa1a6341e4fdbeae26fc4e463c6976
Emilse Arias <emilsearias@gmail.com>
2022-09-15 10:05:19 -0300

    cambie la url de la base de datos

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://localhost:27017/cafeteriac2i';
+// const url = 'mongodb://localhost:27017/cafeteriac2i'; //bd local
+const url = 'mongodb+srv://mdanielacardozo:mtkZQ5FjoyI9ZEIk@clusterdani.7tfbsbj.mongodb.net/cafeteria'; //bdPublicada
+// const url = 'mongodb+srv://comisionc2i:C18PDiw8EH8lfZTr@clustercomision2i.xkgumte.mongodb.net/cafec2i'; //bdPublicada
 
 mongoose.connect(url);
 
