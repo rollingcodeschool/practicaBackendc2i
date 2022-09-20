f3466f76832b45c589921cbb2c2436b3f7af2e6f
Emilse Arias <emilsearias@gmail.com>
2022-09-20 11:15:06 -0300

    agregamos usuarios en la BD

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,9 +1,38 @@
-
+import Usuario from '../models/usuario'
 
 export const login = (req, res)=>{
     res.send('usuario logueado...')
 }
 
-export const crearUsuario = (req,res)=>{
-    res.send('usuario creado')
+export const crearUsuario = async(req,res)=>{
+    try{
+        const {nombre, email, password} = req.body
+
+        //verificar si el email ya existe
+        let usuario = await Usuario.findOne({email});//devulve un null
+        if(usuario){
+            //si el usuario existe
+            return res.status(400).json({
+                mensaje: 'ya existe un usuario con el correo enviado'
+            })
+        }
+
+        //encriptar la contraseña
+        //generar el token
+        //guardamos el nuevo usuario en la BD
+        usuario = new Usuario(req.body)
+        await usuario.save();
+        res.status(201).json({
+            mensaje: 'usuario creado',
+            nombre: usuario.nombre,
+            uid: usuario._id
+        })
+
+    }catch(error){
+        console.log(error)
+        res.status(400).json({
+            mensaje: 'El usuario no pudo ser creado'
+        })
+    }
+
 }
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -3,8 +3,8 @@ import { crearUsuario, login } from '../controllers/usuarios.controllers';
 
 const router = Router();
 
+//agregar las validaciones con express-validator
 router.route('/').post(login);
-
 router.route('/nuevo').post(crearUsuario);
 
 export default router;
