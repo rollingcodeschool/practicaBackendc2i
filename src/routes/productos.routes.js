90e364bb07b1e83e9f38b82b4fa3109bbc7184fc
Emilse Arias <emilsearias@gmail.com>
2022-09-22 12:45:04 -0300

    agregamos la verificacion del token en la ruta de crear producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -7,6 +7,7 @@ import {
   obtenerProducto,
 } from "../controllers/productos.controllers";
 import validarProducto from "../helpers/validacionProducto";
+import validarJWT from "../helpers/validar-jwt";
 
 const router = Router();
 
@@ -14,13 +15,13 @@ const router = Router();
 // dominio+ /apicafe + /productos
 router.route("/productos")
 .get(listarProductos)
-.post(validarProducto, crearProducto);
+.post([validarJWT, validarProducto], crearProducto);
 
 router
   .route("/productos/:id")
   .get(obtenerProducto)
-  .put(editarProducto)
-  .delete(borrarProducto);
+  .put(validarJWT, editarProducto)
+  .delete(validarJWT ,borrarProducto);
 
 export default router;
 
